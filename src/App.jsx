5a0cece58e5ebc127aa3c59899b9e1bfa61f7eb3
gulnarhasanzada <gulnar.hesenzade@gmail.com>
2024-04-23 20:53:46 -0400

    Adding navigation and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import {Box, Container} from '@mui/material';
+import Nav from './components/nav/Nav';
+import AppRouter from './router/AppRouter';
+import { Outlet } from 'react-router-dom';
+
 
 function App() {
 
   return (
-    <>
-    </>
+      <Box display="flex" sx={{flexDirection: 'column'}}>
+        <Nav/>
+        <Container maxWidth="xl">
+          <AppRouter />
+          <Outlet/>
+        </Container>
+      </Box>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+import AnimationIcon from '@mui/icons-material/Animation';
 
 const Nav = () => {
   return (
-    <div>
-      
-    </div>
+    <Box>
+    <AppBar position="static" sx={{display:'flex', flexDirection:'row', justifyContent:'space-between' }}>
+    <Toolbar>
+    <AnimationIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            GMovies
+          </Typography>
+      </Toolbar>
+      <Toolbar>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  </Box>
   )
 }
 
