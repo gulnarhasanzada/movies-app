25c88c67c917ef7b872c922023c11e0f07070fa1
gulnarhasanzada <gulnar.hesenzade@gmail.com>
2024-04-30 12:58:33 -0400

    MovieCard design change

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,13 @@ import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer} from "react-toastify";
 import theme from './Theme';
 import Search from './components/search/Search';
-import { useMoviesContext } from './context/MoviesContext';
 import Footer from './components/footer/Footer';
 
-
 function App() {
-  const {movieDetails} = useMoviesContext();
-  const backdrop = `${import.meta.env.VITE_MOVIES_IMAGE_URL}/${movieDetails.backdrop_path}`
+  
   return (
     <ThemeProvider theme={theme}>
-      <Box className="app" display="flex"sx={{flexDirection: 'column', backgroundImage: `linear-gradient(90deg, rgba(0,0,0,0.95) 30%,  rgba(0,0,0,0.3) 80%, rgba(0,0,0,0.98) 100%), url("${backdrop}")`}}>
+      <Box display="flex" sx={{flexDirection: 'column'}}>
         <Nav/>
         <Search/>
         <ToastContainer />
--- src/components/movies/MovieCard.jsx
+++ src/components/movies/MovieCard.jsx
@@ -10,15 +10,15 @@ import { Link } from 'react-router-dom';
 const MovieCard = ({movie}) => {
   const imgUrl = `${import.meta.env.VITE_MOVIES_IMAGE_URL}/${movie.poster_path}`
   return (
-    <Card sx={{ width: 300, m: 3 }}>
+    <Card sx={{ width: 300, m: 3, backgroundColor:'primary.main' }}>
       <Link to={`/details/${movie.id}`} style={{textDecoration: 'none'}}>
       <CardMedia
         sx={{ height: 450 }}
         image={imgUrl}
         title={movie.title}
       />
-      <CardContent sx={{height:55, overflow:'hidden'}}>
-        <Typography gutterBottom variant="h2" component="div" color="primary" sx={{fontSize:'1rem', fontWeight:'bold'} }>
+      <CardContent sx={{height:55, overflow:'hidden',}}>
+        <Typography gutterBottom variant="h2" component="div" color="white" sx={{fontSize:'1rem',} }>
           {movie.title}
         </Typography>
       </CardContent>
